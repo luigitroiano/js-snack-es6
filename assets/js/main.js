228735c09130b9537110ce9c20387899de1106b8
LuigiTroiano <troiano.luigi8@gmail.com>
2020-12-09 23:52:21 +0100

    Blocco1 Snack3

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,5 +22,43 @@
 // const parola1 = prompt(`Inserisce una parola`);
 // const parola2 = prompt(`Inserisce una parola`);
 
-// const parolaLunga = (word1, word2) => word1 > word2 ? console.log(word2, word1) : console.log(word1, word2);
+// const parolaLunga = (word1, word2) => word1 > word2 ? console.log(word2) + console.log(word1) : console.log(word1) + console.log(word2);
 // parolaLunga(parola1, parola2);
+
+//JSnack3
+// Il software deve chiedere per 5 volte all’utente di inserire un
+// numero. Il programma stampa la somma di tutti i numeri
+// inseriti. Esegui questo programma in due versioni, con il for
+// e con il while.
+////CICLO FOR
+// const userArray = [];
+// for (let i = 0; i < 5; i++) {
+//   const userNumber = Number(prompt('Inserisci un numero'));
+//   userArray.push(userNumber);
+// };
+// console.log(userArray);
+// const somma1 = () => {
+//   let sum = 0;
+//   for (let index = 0; index < userArray.length; index++) {
+//     sum = sum + userArray[index];
+//   };
+//   return sum;
+// }
+// console.log(somma1());
+////CICLO WHILE
+// const userArray2 = [];
+// let y = 0;
+// while (y < 5) {
+//   const userNumber2 = Number(prompt('Inserisci un numero'));
+//   userArray2.push(userNumber2);
+//   y++
+// };
+// console.log(userArray2);
+// const somma2 = () => {
+//   let sum2 = 0;
+//   for (let index2 = 0; index2 < userArray2.length; index2++) {
+//     sum2 = sum2 + userArray2[index2];
+//   };
+//   return sum2;
+// }
+// console.log(somma2());
