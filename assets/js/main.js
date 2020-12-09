7c07c535a2737e92a0c3bf85570acb123c022047
LuigiTroiano <troiano.luigi8@gmail.com>
2020-12-09 22:35:48 +0100

    Blocco1 Snack2

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,12 @@
 //   num1 > num2 ? console.log(num1) : console.log(num2); 
 // numeroMaggiore(numero1, numero2);
 
+// JSnack2
+// L’utente inserisce due parole in successione, con due
+// prompt. Il software stampa prima la parola più corta, poi la
+// parola più lunga.
+// const parola1 = prompt(`Inserisce una parola`);
+// const parola2 = prompt(`Inserisce una parola`);
+
+// const parolaLunga = (word1, word2) => word1 > word2 ? console.log(word2, word1) : console.log(word1, word2);
+// parolaLunga(parola1, parola2);
